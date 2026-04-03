f198a6b642553defaa5e4977b6ff2dfb498605ea
hema972 <hema42426@email.com>
2026-04-03 16:44:16 +0530

    "set"

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './fonts.css';   // ✅ ADD THIS (important for Playfair font)
 import './index.css';
 import App from './App';
 
